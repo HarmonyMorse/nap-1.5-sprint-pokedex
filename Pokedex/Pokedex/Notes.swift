99d7600ce9db7eb9481382f162f53c370f8dc4c2
HarmonyMorse <harmonymorse@icloud.com>
2023-07-07 09:38:42 -0700

    Notes: Added "Storyboards" section

---
+++ Pokedex/Pokedex/Notes.swift
@@ -0,0 +1,36 @@
+//
+//  Notes.swift
+//  Pokedex
+//
+//  Created by Harm on 7/7/23.
+//
+
+import Foundation
+
+/*
+ 
+ //MARK: - Storyboards
+ 
+ PokedexTableVC
+ -Embedded in Nav Controller
+ -Title: Pokedex
+ -Nav item "search" top right
+ -1 section
+ -Items in Section = # of Pokemon in deck
+ -Cell: simple - just shows the name of the Pokemon
+ 
+ PokemonSearchVC
+ -Title: "Pokemon Search" until searched, then name of the Pokemon
+ -Search bar
+ -Label (?) of Pokemon name
+ -ImageView of Pokemon picture
+ -3 Labels for ID, Types, and Abilities
+ -Button for "Save Pokemon"
+ 
+ PokemonDetailVC
+ -Title: name of the Pokemon
+ -Label (?) of Pokemon name
+ -ImageView of Pokemon picture
+ -3 Labels for ID, Types, and Abilities
+ 
+ */
